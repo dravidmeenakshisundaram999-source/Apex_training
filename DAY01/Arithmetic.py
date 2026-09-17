6ae60e0ccc721887a65cb585ea51d617bfa8948c
Dravid.M <dravidmeenakshisundaram999@gmail.com>
2026-09-17 14:58:08 +0530

    Add files via upload

---
+++ DAY01/Arithmetic.py
@@ -0,0 +1,20 @@
+
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Addition =", a + b)
+print("Subtraction =", a - b)
+print("Multiplication =", a * b)
+print("Division =", a / b)
+print("Modulus =", a % b)
+
+output
+
+Enter first number: 3
+Enter second number: 2
+Addition = 5
+Subtraction = 1
+Multiplication = 6
+Division = 1.5
+Modulus = 1
